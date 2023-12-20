a94bf54ac256f329bc8e16933c06f65ec5b95e05
Johan Bijlsma <johan.bijlsma24@gmail.com>
2023-12-20 08:43:12 +0100

    added more features

--- src/views/PosterView.vue
+++ src/views/PosterView.vue
@@ -3,9 +3,18 @@ import Posterdata from '../data/posters.json'
 import { ref } from 'vue'
 
 function goToPrintType() {
-  selection.value.selectedSize = ref(size.value.selectedASize)
   selection.value.selectedSizeParent = ref(size.value.selectedSizeParent)
+  selection.value.selectedASize = ref(size.value.selectedASize)
+  selection.value.selectedBSize = ref(size.value.selectedBSize)
+  selection.value.selectedSpecialSize = ref(size.value.selectedSpecialSize)
   progress.value = ref(2)
+}
+function goToMaterial() {
+  selection.value.selectedPrintParent = ref(printType.value.selectedPrintParent)
+  selection.value.selectedPMS = ref(printType.value.selectedPMS)
+  selection.value.selectedFullColor = ref(printType.value.selectedFullColor)
+  selection.value.selectedFullColorBacksideAbri = ref(printType.value.selectedFullColorBacksideAbri)
+  progress.value = ref(3)
 }
 
 const progress = ref(1)
@@ -158,9 +167,241 @@ const size = ref({
     }
   ]
 })
+
+const material = ref({
+  selectedMaterialParent: '',
+  fluoriserend_neon: [
+    {
+      slug: '95gr_neon_papier_geel',
+      name: '95 g/m² Neon yellow paper',
+      nullable: false,
+      eco: false,
+      parent: '1gr_fluoriserend_neon',
+      enrichments: [
+        {
+          propertySlug: 'color',
+          optionSlug: '#fffa30'
+        }
+      ]
+    },
+    {
+      slug: '95gr_neon_papier_groen',
+      name: '95 g/m² Neon green paper',
+      nullable: false,
+      eco: false,
+      parent: '1gr_fluoriserend_neon',
+      enrichments: [
+        {
+          propertySlug: 'color',
+          optionSlug: '#5aef00'
+        }
+      ]
+    },
+    {
+      slug: '95gr_neon_papier_magenta',
+      name: '95 g/m² Neon magenta paper',
+      nullable: false,
+      eco: false,
+      parent: '1gr_fluoriserend_neon',
+      enrichments: [
+        {
+          propertySlug: 'color',
+          optionSlug: '#ff00c7'
+        }
+      ]
+    },
+    {
+      slug: '95gr_neon_papier_oranje',
+      name: '95 g/m² Neon orange paper',
+      nullable: false,
+      eco: false,
+      parent: '1gr_fluoriserend_neon',
+      enrichments: [
+        {
+          propertySlug: 'color',
+          optionSlug: '#ff8300'
+        }
+      ]
+    }
+  ],
+  waterproof: [
+    {
+      slug: '115gr_blueback',
+      name: '115 g/m² Blueback - water resistant',
+      nullable: false,
+      eco: false,
+      parent: '1gr_waterproof',
+      enrichments: [
+        {
+          propertySlug: 'thickness',
+          optionSlug: '0.15'
+        },
+        {
+          propertySlug: 'color',
+          optionSlug: '#ffffff'
+        },
+        {
+          propertySlug: 'weight',
+          optionSlug: '115'
+        }
+      ]
+    },
+    {
+      slug: '170gr_synaps',
+      name: '170 g/m² Synaps® (Rip-proof and water proof) ',
+      nullable: false,
+      eco: false,
+      parent: '1gr_waterproof',
+      enrichments: [
+        {
+          propertySlug: 'color',
+          optionSlug: '#ffff'
+        }
+      ]
+    },
+    {
+      slug: '115gr_blueback',
+      name: '115 g/m² Blueback - water resistant',
+      nullable: false,
+      eco: false,
+      parent: '1gr_waterproof',
+      enrichments: [
+        {
+          propertySlug: 'thickness',
+          optionSlug: '0.15'
+        },
+        {
+          propertySlug: 'color',
+          optionSlug: '#ffffff'
+        },
+        {
+          propertySlug: 'weight',
+          optionSlug: '115'
+        }
+      ]
+    }
+  ]
+})
+
+const printType = ref({
+  selectedPrintParent: 'pms',
+  selectedFullColor: '',
+  selectedPMS: '',
+  selectedFullColorBacksideAbri: '',
+  pms: [
+    {
+      slug: '10_pms_black',
+      name: '1/0 PMS black',
+      nullable: false,
+      eco: false,
+      pages: [
+        {
+          numberOfColors: 1,
+          colorspace: 'indexed'
+        }
+      ],
+      parent: 'pms',
+      enrichments: []
+    }
+  ],
+  FullColor: [
+    {
+      slug: 'full_color',
+      name: 'Full color',
+      nullable: false,
+      eco: false,
+      pages: [
+        {
+          numberOfColors: 1,
+          colorspace: 'rgb'
+        }
+      ],
+      parent: 'full_color',
+      enrichments: []
+    },
+    {
+      slug: '40',
+      name: '4/0 Full colour',
+      nullable: false,
+      eco: false,
+      pages: [
+        {
+          numberOfColors: 4,
+          colorspace: 'cmyk'
+        }
+      ],
+      parent: 'full_color',
+      enrichments: []
+    }
+  ],
+  Full_color_backside_abri: [
+    {
+      slug: '42',
+      name: '4/2',
+      nullable: false,
+      eco: false,
+      pages: [
+        {
+          numberOfColors: 4,
+          colorspace: 'cmyk'
+        },
+        {
+          numberOfColors: 2,
+          colorspace: 'indexed'
+        }
+      ],
+      parent: 'full_color_backside_abri',
+      enrichments: []
+    },
+    {
+      slug: '43',
+      name: '4/3',
+      nullable: false,
+      eco: false,
+      pages: [
+        {
+          numberOfColors: 4,
+          colorspace: 'cmyk'
+        },
+        {
+          numberOfColors: 3,
+          colorspace: 'indexed'
+        }
+      ],
+      parent: 'full_color_backside_abri',
+      enrichments: []
+    },
+    {
+      slug: '44_abri',
+      name: '4/4 (mirrored)',
+      nullable: false,
+      eco: false,
+      pages: [
+        {
+          numberOfColors: 4,
+          colorspace: 'cmyk'
+        },
+        {
+          numberOfColors: 4,
+          colorspace: 'cmyk'
+        }
+      ],
+      parent: 'full_color_backside_abri',
+      enrichments: []
+    }
+  ]
+})
 const selection = ref({
   selectedSize: '',
-  selectedSizeParent: ''
+  selectedASize: '',
+  selectedBSize: '',
+  selectedSpecialSize: '',
+  selectedSizeParent: '',
+  selectedPrintParent: '',
+  selectedFullColor: '',
+  selectedPMS: '',
+  selectedFullColorBacksideAbri: '',
+  selectedPrintType: ''
 })
 const orders = ref([])
 </script>
@@ -207,7 +448,7 @@ const orders = ref([])
           />
           Specials</label
         >
-        <div class="size-dropdown">
+        <div class="filter-dropdown">
           <template v-if="size.selectedSizeParent === 'a-formaten'">
             <select name="size" id="Asize" v-model="size.selectedASize">
               <option v-for="(Asizes, index) in size.Aformats" :key="index" :value="Asizes.slug">
@@ -236,82 +477,243 @@ const orders = ref([])
         </div>
       </div>
       <div class="button-container">
-        <button class="primary" @click="goToPrintType">Go to Print type</button>
+        <button class="primary" @click="goToPrintType">Choose your Print type</button>
       </div>
     </template>
+
     <template v-if="progress.value === 2">
-      <div class="step-block step-block-small step-1-small">
-        <h2 class="step-title">1. Size</h2>
-        <p class="selected-size selected-size-parent">
-          {{ selection.selectedSizeParent }}
-        </p>
-        <p class="selected-size">{{ selection.selectedSize }}</p>
+      <div class="progress-container">
+        <div class="step-block step-block-small step-1-small">
+          <h2 class="step-title">1. Size</h2>
+          <p class="selected-type selected-type-parent">
+            {{ selection.selectedSizeParent }}
+          </p>
+          <p class="selected-type">
+            {{ selection.selectedASize }} {{ selection.selectedBSize }}
+            {{ selection.selectedSpecialSize }}
+          </p>
+        </div>
       </div>
       <aside class="step2">
         <h2 class="step-title">2. Print Type</h2>
       </aside>
       <div class="step-block step-2-block">
-        <label for="a-formaten">
+        <label for="pms">
           <input
             type="radio"
-            name="poster-size"
-            id="a-formaten"
-            value="a-formaten"
-            v-model="size.selectedSizeParent"
-          />A-formaten</label
+            name="printer-type"
+            id="pms"
+            value="pms"
+            v-model="printType.selectedPrintParent"
+          />PMS</label
         >
-        <label for="b-formaten">
+        <label for="fullColour">
           <input
             type="radio"
-            name="poster-size"
-            id="b-formaten"
-            value="b-formaten"
-            v-model="size.selectedSizeParent"
-          />B-formaten</label
+            name="printer-type"
+            id="fullColour"
+            value="fullColour"
+            v-model="printType.selectedPrintParent"
+          />Full Color</label
         >
-        <label for="specials"
+        <label for="Full_color_backside_abri"
           ><input
             type="radio"
-            name="poster-size"
-            id="specials"
-            value="specials"
-            v-model="size.selectedSizeParent"
+            name="printer-type"
+            id="Full_color_backside_abri"
+            value="Full_color_backside_abri"
+            v-model="printType.selectedPrintParent"
           />
-          Specials</label
+          Full Color Backside Abri</label
         >
-        <div class="size-dropdown">
-          <template v-if="size.selectedSizeParent === 'a-formaten'">
-            <select name="size" id="Asize" v-model="size.selectedASize">
-              <option v-for="(Asizes, index) in size.Aformats" :key="index" :value="Asizes.slug">
-                {{ Asizes.name }}
+        <div class="filter-dropdown">
+          <template v-if="printType.selectedPrintParent === 'pms'">
+            <select name="print-type" id="Pms" v-model="printType.selectedPms">
+              <option v-for="(Pms, index) in printType.pms" :key="index" :value="Pms.slug">
+                {{ Pms.name }}
               </option>
             </select>
           </template>
-          <template v-if="size.selectedSizeParent === 'b-formaten'">
-            <select name="size" id="Bsize" v-model="size.selectedBSize">
-              <option v-for="(Bsizes, index) in size.Bformats" :key="index" :value="Bsizes.slug">
-                {{ Bsizes.name }}
-              </option>
-            </select>
-          </template>
-          <template v-if="size.selectedSizeParent === 'specials'">
-            <select name="size" id="Specialsize" v-model="size.selectedSpecialSize">
+          <template v-if="printType.selectedPrintParent === 'fullColour'">
+            <select name="print-type" id="fullColour" v-model="printType.selectedFullColor">
               <option
-                v-for="(specials, index) in size.Bformats"
+                v-for="(Fullcolor, index) in printType.FullColor"
                 :key="index"
-                :value="specials.slug"
+                :value="Fullcolor.slug"
               >
-                {{ specials.name }}
+                {{ Fullcolor.name }}
+              </option>
+            </select>
+          </template>
+          <template v-if="printType.selectedPrintParent === 'Full_color_backside_abri'">
+            <select
+              name="print-type"
+              id="FullColorBacksideAbri"
+              v-model="printType.selectedFullColorBacksideAbri"
+            >
+              <option
+                v-for="(FullcolorbgAbri, index) in printType.Full_color_backside_abri"
+                :key="index"
+                :value="FullcolorbgAbri.slug"
+              >
+                {{ FullcolorbgAbri.name }}
               </option>
             </select>
           </template>
         </div>
       </div>
       <div class="button-container">
-        <button class="primary" @click="goToPrintType">Go to Print type</button>
+        <button class="primary" @click="goToMaterial">Choose your Material</button>
       </div>
     </template>
-    <pre>{{ Posterdata }}</pre>
+
+    <template v-if="progress.value === 3">
+      <div class="progress-container">
+        <div class="step-block step-block-small step-1-small">
+          <h2 class="step-title">1. Size</h2>
+          <p class="selected-type selected-type-parent">
+            {{ selection.selectedSizeParent }}
+          </p>
+          <p class="selected-type">
+            {{ selection.selectedASize }} {{ selection.selectedBSize }}
+            {{ selection.selectedSpecialSize }}
+          </p>
+        </div>
+        <div class="step-block step-block-small step-2-small">
+          <h2 class="step-title">2. Print Type</h2>
+          <p class="selected-type selected-type-parent">
+            {{ selection.selectedPrintParent }}
+          </p>
+          <p class="selected-type">
+            {{ selection.selectedPMS }} {{ selection.selectedFullColor }}
+            {{ selection.selectedFullColorBacksideAbri }}
+          </p>
+        </div>
+      </div>
+      <aside class="step3">
+        <h2 class="step-title">3. Material</h2>
+      </aside>
+      <div class="step-block step-3-block">
+        <label for="95gr_neon_papier_geel">
+          <input
+            type="radio"
+            name="material-type"
+            id="95gr_neon_papier_geel"
+            value="95gr_neon_papier_geel"
+            v-model="material.select"
+          />95gr neon papier geel</label
+        >
+        <label for="115gr_blueback">
+          <input
+            type="radio"
+            name="material-type"
+            id="115gr_blueback"
+            value="115gr_blueback"
+            v-model="material.select"
+          />
+          115gr blueback</label
+        >
+        <label for="135gr_gesatineerd_mc"
+          ><input
+            type="radio"
+            name="material-type"
+            id="135gr_gesatineerd_mc"
+            value="135gr_gesatineerd_mc"
+            v-model="material.select"
+          />
+          135gr gesatineerd mc</label
+        >
+        <label for="170gr_silk_mc"
+          ><input
+            type="radio"
+            name="material-type"
+            id="170gr_silk_mc"
+            value="170gr_silk_mc"
+            v-model="material.select"
+          />
+          170gr silk mc</label
+        >
+        <label for="170gr_synaps"
+          ><input
+            type="radio"
+            name="material-type"
+            id="170gr_synaps"
+            value="170gr_synaps"
+            v-model="material.select"
+          />
+          170gr synaps</label
+        >
+        <label for="230gr_premium_matt"
+          ><input
+            type="radio"
+            name="material-type"
+            id="230gr_premium_matt"
+            value="230gr_premium_matt"
+            v-model="material.select"
+          />
+          230gr premium matt</label
+        >
+        <label for="250gr_gesatineerd_mc"
+          ><input
+            type="radio"
+            name="material-type"
+            id="250gr_gesatineerd_mc"
+            value="250gr_gesatineerd_mc"
+            v-model="material.select"
+          />
+          250gr gesatineerd mc</label
+        >
+        <label for="250gr_silk_mc"
+          ><input
+            type="radio"
+            name="material-type"
+            id="250gr_silk_mc"
+            value="250gr_silk_mc"
+            v-model="material.select"
+          />
+          250gr silk mc</label
+        >
+
+        <div class="filter-dropdown">
+          <template v-if="printType.selectedPrintParent === 'pms'">
+            <select name="print-type" id="Pms" v-model="printType.selectedPms">
+              <option v-for="(Pms, index) in printType.pms" :key="index" :value="Pms.slug">
+                {{ Pms.name }}
+              </option>
+            </select>
+          </template>
+          <template v-if="printType.selectedPrintParent === 'fullColour'">
+            <select name="print-type" id="fullColour" v-model="printType.selectedFullColor">
+              <option
+                v-for="(Fullcolor, index) in printType.FullColor"
+                :key="index"
+                :value="Fullcolor.slug"
+              >
+                {{ Fullcolor.name }}
+              </option>
+            </select>
+          </template>
+          <template v-if="printType.selectedPrintParent === 'Full_color_backside_abri'">
+            <select
+              name="print-type"
+              id="FullColorBacksideAbri"
+              v-model="printType.selectedFullColorBacksideAbri"
+            >
+              <option
+                v-for="(FullcolorbgAbri, index) in printType.Full_color_backside_abri"
+                :key="index"
+                :value="FullcolorbgAbri.slug"
+              >
+                {{ FullcolorbgAbri.name }}
+              </option>
+            </select>
+          </template>
+        </div>
+      </div>
+      <div class="button-container">
+        <button class="primary" @click="goToMaterial">Choose your finish</button>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -402,10 +804,11 @@ h2.step-title {
       box-shadow: 0 0 5px var(--print-primary) / 0.5;
     }
   }
-  & .size-dropdown {
+  & .filter-dropdown {
     width: 100%;
     & select {
       width: 100%;
+      min-height: 2rem;
     }
   }
   &.step-block-small {
@@ -415,7 +818,7 @@ h2.step-title {
     max-width: fit-content;
     border-top: var(--print-primary) 4px solid;
     padding-top: 1rem;
-    & p.selected-size {
+    & p.selected-type {
       margin-block: 00.25rem;
       font-size: 1.375rem;
       font-style: normal;
@@ -424,6 +827,13 @@ h2.step-title {
       text-transform: uppercase;
     }
   }
+}
+
+.progress-container {
+  display: flex;
+  flex-wrap: wrap;
+  grid-column-start: 2;
+  gap: 8px;
 }
 
 .button-container {
